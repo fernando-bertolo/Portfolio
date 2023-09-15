f7a46d7167427a02f212777c16c1c3b5fb8a8ddf
fernando-bertolo <fernando.bertolo.jr123@gmail.com>
2023-09-14 21:58:07 -0300

    Ajustando frontend

--- frontend/src/components/Home/indexHome.jsx
+++ frontend/src/components/Home/indexHome.jsx
@@ -1,15 +1,18 @@
 import "./indexHome.css";
 
-function Home(){
-    return(
-        <div className="App">
-            <nav className="navBar">
-                <p>teste</p>
-            </nav>
-        </div>
-
-
-    )
+function Home() {
+  return (
+    <div className="App">
+      <nav className="navBar">
+        <ul>
+          <li>Home</li>
+          <li>Currículo</li>
+          <li>Certificados</li>
+          <li>Projetos</li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default Home;
