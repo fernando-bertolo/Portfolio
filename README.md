7bc416eb4c7b2aa3836fc8bfc425ab3c1bc95005
fernando-bertolo <fernando.bertolo.jr123@gmail.com>
2023-09-18 16:11:13 -0300

    Construindo o menu lateral do portfolio

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Portfolio
 Projeto feito em ReactJS. Dependencias instaladas:
 ```
-npm install styled-components react-icons
+npm install styled-components @react-icons/all-files --save
 ```
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import {Route, Routes} from "react-router-dom";
 import Home from "./components/Home/indexHome.jsx";
-import Contato from "./components/Contato/indexContato.jsx";
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<Home/>}/>
-      <Route path="/contato" element={<Contato/>}/>
+      <Route path="/Sobre-Mim"/>
+      <Route path="/curriculo"/>
+      <Route path="/certificados"/>
+      <Route path="/projetos"/>
+      <Route path="/contato"/>
     </Routes>
   );
 }
