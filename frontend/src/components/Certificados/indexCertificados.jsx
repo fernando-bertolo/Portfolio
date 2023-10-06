f7d966fb3fc95bb22264fb321c1012aafc35545f
fernando-bertolo <fernando.bertolo.jr123@gmail.com>
2023-10-05 23:52:26 -0300

    Finalizando tela de certificados

--- frontend/src/components/Certificados/indexCertificados.jsx
+++ frontend/src/components/Certificados/indexCertificados.jsx
@@ -11,6 +11,7 @@ import { styled } from "styled-components";
 import ImageCertificado1 from "../Certificados/Images/ImageCertificado1.png";
 import ImageCertificado2 from "../Certificados/Images/ImageCertificado2.png";
 import ImageCertificado3 from "../Certificados/Images/ImageCertificado3.png";
+import ImageCertificado4 from "../Certificados/Images/CertificadoSkill.png";
 
 const DivTelaMain = styled.div`
   width: 100vw;
@@ -30,6 +31,10 @@ const DivMainTextoCertificados = styled.div`
 const TextoCertificado = styled.p`
   color: #fff;
   font-size: 2vw;
+
+  @media(max-width: 768px){
+    font-size:  clamp(4vw, 5vw, 5.5vw);
+  }
 `;
 
 const DivMainConteudo = styled.div`
@@ -44,14 +49,26 @@ const DivMainCertificados = styled.div`
   width: 60%;
   height: 100%;
   /* background-color: red; */
-  display: flex;
-  justify-content: space-around;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
   gap: 1rem;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+
+  @media(min-width: 769px) and (max-width: 1100px){
+    overflow: auto;
+  }
+
+  @media(max-width: 768px){
+    display: flex;
+    flex-direction: column;
+    overflow: auto;
+  }
 `;
 
+
 const DivCertificados = styled.div`
-  width: 35%;
-  height: 30%;
+  width: 100%;
+  height: 70%;
   border-radius: 20px;
   background-color: gray;
 
@@ -64,7 +81,7 @@ const ImagensCertificados = styled.img`
   width: 100%;
   height: 100%;
   border-radius: 20px;
-  object-fit: cover; // Dimensiona a imagem dentro da DIV
+  //object-fit: cover; // Dimensiona a imagem dentro da DIV
 `;
 
 const LinksCertificado = styled.a``;
@@ -91,35 +108,50 @@ function Certificados() {
         </DivMainTextoCertificados>
         <DivMainConteudo>
           <DivMainCertificados>
-            {/* Imagem 1 */}
-            <DivCertificados>
-              <LinksCertificado
-                href="https://cursos.alura.com.br/certificate/ac8c4777-74b0-4c72-9373-381b67ce7d08?lang=pt_BR"
-                target="_blank"
-              >
-                <ImagensCertificados src={ImageCertificado1} />
-              </LinksCertificado>
-            </DivCertificados>
+              {/* Imagem 1 */}
+              <DivCertificados>
+                <LinksCertificado
+                  href="https://cursos.alura.com.br/certificate/ac8c4777-74b0-4c72-9373-381b67ce7d08?lang=pt_BR"
+                  target="_blank"
+                >
+                  <ImagensCertificados src={ImageCertificado1} />
+                </LinksCertificado>
+              </DivCertificados>
 
-            {/* Imagem 2 */}
-            <DivCertificados>
-              <LinksCertificado
-                href="https://cursos.alura.com.br/certificate/09b4d5d9-95b7-46af-ae44-0524f17dc537?lang=pt_BR"
-                target="_blank"
-              >
-                <ImagensCertificados src={ImageCertificado2} />
-              </LinksCertificado>
-            </DivCertificados>
 
-            {/* Imagem 3 */}
-            <DivCertificados>
-              <LinksCertificado
-                href="https://cursos.alura.com.br/certificate/1ed3787c-e0d0-4663-a145-febf432091df?lang=pt_BR"
-                target="_blank"
-              >
-                <ImagensCertificados src={ImageCertificado3} />
-              </LinksCertificado>
-            </DivCertificados>
+                {/* Imagem 2 */}
+                <DivCertificados>
+                <LinksCertificado
+                  href="https://cursos.alura.com.br/certificate/09b4d5d9-95b7-46af-ae44-0524f17dc537?lang=pt_BR"
+                  target="_blank"
+                >
+                  <ImagensCertificados src={ImageCertificado2} />
+                </LinksCertificado>
+              </DivCertificados>
+
+              {/* Imagem 3 */}
+              <DivCertificados>
+                <LinksCertificado
+                  href="https://cursos.alura.com.br/certificate/1ed3787c-e0d0-4663-a145-febf432091df?lang=pt_BR"
+                  target="_blank"
+                >
+                  <ImagensCertificados src={ImageCertificado3} />
+                </LinksCertificado>
+              </DivCertificados>
+
+
+            {/* Seção 2 Certificados */}
+
+              {/* Imagem 4 */}
+              <DivCertificados>
+                <LinksCertificado
+                  href="https://app.skilled.com.br/Certificado?Hash=C4491812BA1AE43D"
+                  target="_blank"
+                >
+                 <ImagensCertificados src={ImageCertificado4} />
+                </LinksCertificado>
+              </DivCertificados>
+
           </DivMainCertificados>
         </DivMainConteudo>
       </DivTelaMain>
